d8e1fe531d525fcb4e6e0cb7d7ccceb9e1ace01c
JazKarit <jaskrits.singh@gmail.com>
2021-02-23 19:06:19 -0700

    Converted to class and cleaned bugs

--- basic_scrapers/google_map_scraper.py
+++ basic_scrapers/google_map_scraper.py
@@ -1,79 +1,107 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import csv
 
-browser = webdriver.Chrome('C:/Users/jazka/Documents/chromedriver.exe')
 
+class GoogleMapWebScraper:
 
-def get_sources(url, term):
-    """
-    Scrape sources from the given google maps url of a search at a given location. For each company, 
-    get the name, service provided, location, and phone number if available.
-    """
-    # url = 'https://www.google.com/maps/search/carpentry/@-19.8292149,45.5268368,6.91z'
+    def __init__(self, search_terms=[], coordinates=(-19.8292149, 45.5268368), zoom=6.91, phone_extension='+261'):
+        self.search_terms = search_terms
+        # self.fields_to_collect = fields_to_collect
+        self.coordinates = coordinates
+        self.zoom = zoom
+        self.url_list = self.get_urls()
+        self.browser = webdriver.Chrome(
+            'C:/Users/jazka/Documents/chromedriver.exe')
+        self.business_lst = []
+        self.phone_extension = phone_extension
 
-    browser.get(url)
+    def get_urls(self):
+        """
+        Convert the terms and coordinates into urls
+        """
+        urls = []
+        for term in terms:
 
-    time.sleep(5)
+            # Convert spaces to + signs as used in Google Maps
+            term_lst = term.split(' ')
+            term = ''
+            for term_part in term_lst:
+                term += term_part + '+'
 
-    # Zoom in so that "Search this area" button will show up
-    browser.find_element_by_id('widget-zoom-in').click()
+            urls.append('https://www.google.com/maps/search/' +
+                        term + '/@' + str(self.coordinates[0]) + ',' + str(self.coordinates[1]) + ',' + str(self.zoom) + 'z')
 
-    time.sleep(2)
+        return urls
 
-    # Must hit  "Search this area", otherwise Google defaults to local locations
-    browser.find_element_by_class_name('widget-search-this-area-inner').click()
+    def get_sources(self):
+        """
+        Scrape sources from the given google maps url of a search at a given location. For each company, 
+        get the name, service provided, location, and phone number if available.
+        """
+        business_lst = []
+        i = 0
+        for url in self.url_list:
+            print(url)
+            self.browser.get(url)
 
-    time.sleep(2)
+            time.sleep(7)
 
-    # For future clicking on each company and getting more info
-    # results = browser.find_elements_by_class_name('section-result-content')
-    # for result in results:
-    #     result.click()
-    #     time.sleep(2)
-    #     htmlstring = browser.page_source
-    #     company_soup = BeautifulSoup(htmlstring, "html.parser")
-    #     print(company_soup.find(class_='section-layout').text)
-    #     browser.find_element_by_class_name(
-    #         'section-back-to-list-button').click()
-    #     time.sleep(2)
+            # Zoom in so that "Search this area" button will show up
+            self.browser.find_element_by_id('widget-zoom-in').click()
 
-    htmlstring = browser.page_source
+            time.sleep(3)
 
-    soup = BeautifulSoup(htmlstring, "html.parser")
+            # Must hit  "Search this area", otherwise Google defaults to local locations
+            self.browser.find_element_by_class_name(
+                'widget-search-this-area-inner').click()
 
-    results = soup.find_all(class_='section-result-content')
+            time.sleep(5)
 
-    business_dict = {}
+            htmlstring = self.browser.page_source
+            soup = BeautifulSoup(htmlstring, "html.parser")
+            results = soup.find_all(class_='section-result-content')
 
-    for result in results:
-        name = result.find(class_='section-result-title').text
-        service = result.find(class_='section-result-details').text
+            for result in results:
+                name = result.find(class_='section-result-title').text
+                service = result.find(class_='section-result-details').text
 
-        # Default to the seach term if no service provided was found
-        if service == '':
-            service = term
+                # Default to the seach term if no service provided was found
+                if service == '':
+                    service = search_terms[i]
 
-        location = result.find(class_='section-result-location').text
-        phone = result.find(
-            class_='section-result-info section-result-phone-number').text
-        business_dict[name] = [service, location, phone]
-        # print(name)
-        # print(service)
-        # print(location)
-        # print(phone)
-        # print()
-    return business_dict
+                location = result.find(class_='section-result-location').text
+                phone = result.find(
+                    class_='section-result-info section-result-phone-number').text
+
+                # Get rid of phone numbers with the wrong extension. These may come from your ip address location!
+                if phone.strip()[0:len(self.phone_extension)] != self.phone_extension:
+                    phone = ''
+
+                business_lst.append(
+                    {'name': name, 'service': service, 'location': location, 'phone': phone})
+            self.business_lst = business_lst
+            i += 1
+        return business_lst
+
+    def write_data_to_csv(self, filename):
+        """
+        Writes the data collected in get_sources() to a csv file
+        """
+        csv_columns = ['name', 'service', 'location', 'phone']
+        try:
+            with open(filename, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                for data in self.business_lst:
+                    writer.writerow(data)
+        except IOError:
+            print("I/O error")
 
 
 terms = ['Transportation', 'Construction', 'Furniture', 'Office', 'school supplies', 'Material', 'Concrete',
          'Mud brick', 'Thatched roofing', 'Truck', 'Freight', 'train', 'Utility', 'Electricity', 'Sewage']
-# terms = ['freight']
-for term in terms:
-    # print('\n\n')
-    # print(term)
-    # print()
-    business_dict = get_sources('https://www.google.com/maps/search/' +
-                                term + '/@-19.8292149,45.5268368,6.91z', term)
-
-    print(business_dict)
+scraper = GoogleMapWebScraper(terms)
+scraper.get_sources()
+scraper.write_data_to_csv('google_maps_data_Transport.csv')
